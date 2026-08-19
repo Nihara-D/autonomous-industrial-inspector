6b49b4424506b2233741445c0d90fab03e023885
Nihara Dayarathne <rdniharash@gmail.com>
2026-08-19 13:35:50 +0530

    fix(moveit): explicitly bind ompl planning plugin namespace in launch parameters

--- src/inspector_moveit_config/launch/moveit.launch.py
+++ src/inspector_moveit_config/launch/moveit.launch.py
@@ -15,16 +15,26 @@ def generate_launch_description():
         .robot_description_kinematics(file_path="config/kinematics.yaml")
         .joint_limits(file_path="config/joint_limits.yaml")
         .trajectory_execution(file_path="config/moveit_controllers.yaml")
-        .planning_pipelines(pipelines=["ompl"])
+        .planning_pipelines(default_planning_pipeline="ompl", pipelines=["ompl"])
         .to_moveit_configs()
     )
+
+    # MoveGroup Parameters Definition
+    move_group_params = [
+        moveit_config.to_dict(),
+        {
+            'planning_pipelines': ['ompl'],
+            'default_planning_pipeline': 'ompl',
+            'ompl.planning_plugin': 'ompl_interface/OMPLPlanner',
+        }
+    ]
 
     # MoveGroup Node
     move_group_node = Node(
         package="moveit_ros_move_group",
         executable="move_group",
         output="screen",
-        parameters=[moveit_config.to_dict()]
+        parameters=move_group_params
     )
 
     # Static TF Publisher
